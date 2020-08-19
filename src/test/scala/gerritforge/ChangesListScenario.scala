c3b932b22c48e6d997a497a389bc1f7fc45bb28a
Luca Milanesio <luca.milanesio@gmail.com>
2020-08-19 13:39:12 +0100

    Add full requests for a change detail page

--- src/test/scala/gerritforge/ChangesListScenario.scala
+++ src/test/scala/gerritforge/ChangesListScenario.scala
@@ -1,5 +1,7 @@
 package gerritforge
 
+import java.net.HttpURLConnection
+
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
@@ -8,10 +10,12 @@ import io.circe._
 import io.circe.parser._
 import io.circe.generic.auto._
 import io.circe.syntax._
+import jodd.net.HttpStatus
+import HttpURLConnection._
 
 import scala.util.Random
 
-case class ChangeDetail(_number: Int, project: String) {
+case class ChangeDetail(_number: Int, project: String, change_id: String) {
   lazy val url = s"/c/${project}/+/${_number}/"
 }
 
@@ -27,7 +31,9 @@ object ChangesListScenario {
 
   val random = new Random
 
-  def listChanges(authCookie: Option[String] = None) =
+  def listChanges(authCookie: Option[String] = None) = {
+    val checkStatus = status.in(authCookie.fold(Seq(HTTP_FORBIDDEN))(_ => Seq(HTTP_OK, HTTP_NO_CONTENT)))
+
     authCookie
       .fold(exec(flushSessionCookies)) { auth =>
         exec(addCookie(Cookie("GerritAccount", auth)))
@@ -48,11 +54,54 @@ object ChangesListScenario {
                   .transform(decode[List[ChangeDetail]](_))
                   .transform(_.right.get)
                   .transform(changes => changes(random.nextInt(changes.size)))
-                  .transform(_.url)
-                  .saveAs("changeUrl"))))
+                  .saveAs("changeDetail"))))
+      .exec { session =>
+        val change = session("changeDetail").as[ChangeDetail]
+        session
+          .set("changeUrl", change.url)
+          .set("id", s"${change.project}~${change._number}")
+          .set("changeNum", change._number)
+          .set("changeId", change.change_id)
+          .set("project", change.project)
+      }
+      .pause(2 seconds)
       .exec(
         http("get change details")
           .get("${changeUrl}")
-      )
-      .pause(2 seconds)
+          .headers(restApiHeader)
+          .resources(
+            http("check account capabilities")
+              .get("/accounts/self/capabilities")
+              .check(checkStatus),
+            http("fetch comments")
+              .get("/changes/${id}/comments"),
+            http("fetch robot-comments")
+              .get("/changes/${id}/robotcomments"),
+            http("get change details")
+              .get("/changes/${id}/detail?O=916314"),
+            http("get draft comments")
+              .get("/changes/${id}/drafts")
+              .check(checkStatus),
+            http("get download commands")
+              .get("/changes/${id}/edit/?download-commands=true")
+              .check(checkStatus),
+            http("get project config")
+              .get("/projects/${project}/config"),
+            http("get available actions")
+              .get("/changes/${id}/revisions/1/actions"),
+            http("get list of reviewed files")
+              .get("/changes/${id}/revisions/1/files?reviewed")
+              .check(checkStatus),
+            http("check if change is mergeable")
+              .get("/changes/${id}/revisions/current/mergeable"),
+            http("get related changes")
+              .get("/changes/${id}/revisions/1/related"),
+            http("get cherry picks")
+              .get("/changes/?O=a&q=project%3A${project}%20change%3A${changeId}%20-change%3A${changeNum}%20-is%3Aabandoned"),
+            http("get conflicting changes")
+              .get("/changes/?O=a&q=status%3Aopen%20conflicts%3A${changeNum}"),
+            http("check for other changes submittable together")
+              .get("/changes/${id}/submitted_together?o=NON_VISIBLE_CHANGES"))
+        )
+  }
 }
