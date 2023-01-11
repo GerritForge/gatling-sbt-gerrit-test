12348fd0881a98b7763d234e35050db89b7b8bd5
Daniele Sassoli <dani@danieles-mbp.home>
2023-01-11 21:27:00 +0000

    Refactoring GerritGitSimulation so that the methods themselves don't return Options anymore
    Change-Id: Ifea368d9258c5cc54976a70112fbf080b1b0d8b5

--- src/test/scala/gerritforge/GerritRestSimulation.scala
+++ src/test/scala/gerritforge/GerritRestSimulation.scala
@@ -19,8 +19,8 @@ class GerritRestSimulation extends Simulation {
       http
         .baseUrl(url)
         .inferHtmlResources(
-          BlackList(""".*\.js""", """.*\.css""", """.*\.ico""", """.*\.woff2""", """.*\.png"""),
-          WhiteList()
+          AllowList(),
+          DenyList(""".*\.js""", """.*\.css""", """.*\.ico""", """.*\.woff2""", """.*\.png""")
         )
         .acceptHeader("*/*")
         .acceptEncodingHeader("gzip, deflate")
@@ -45,19 +45,24 @@ class GerritRestSimulation extends Simulation {
     )
   )
 
-  val anonymousUserChangeList = List(scenario("Anonymous user").exec(listChanges(testConfig.project)))
-  val authenticatedChangeList: List[ScenarioBuilder] = List(scenario("Regular user")
-    .feed(randomReview)
-    .exec(listChanges(testConfig.project, testConfig.accountCookie, testConfig.xsrfToken))
+  val anonymousUserChangeList = List(
+    scenario("Anonymous user").exec(listChanges(testConfig.project))
+  )
+  val authenticatedChangeList: List[ScenarioBuilder] = List(
+    scenario("Regular user")
+      .feed(randomReview)
+      .exec(listChanges(testConfig.project, testConfig.accountCookie, testConfig.xsrfToken))
   )
 
-  val scenarios =  if (testConfig.restRunAnonymousUser) authenticatedChangeList ++ anonymousUserChangeList else authenticatedChangeList
+  val scenarios =
+    if (testConfig.restRunAnonymousUser) authenticatedChangeList ++ anonymousUserChangeList
+    else authenticatedChangeList
 
   require(httpProtocol.isDefined, "GERRIT_HTTP_URL must be defined to run REST-API simulation")
 
-
   setUp(
-    scenarios.map(_.inject(
-      rampConcurrentUsers(1) to testConfig.numUsers during (testConfig.duration)))
+    scenarios.map(
+      _.inject(rampConcurrentUsers(1) to testConfig.numUsers during (testConfig.duration))
+    )
   ).protocols(httpProtocol)
 }
