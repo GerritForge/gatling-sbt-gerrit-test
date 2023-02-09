cae42d1fa329010f549830b03b871f83b883ba14
Marcin Czech <maczech@gmail.com>
2023-02-09 21:02:43 +0100

    [DO NOT MERGE] Do not use force push to create branches
    Change-Id: Ib0459c24768bbb14a4077aa13796cc77dceb51b7

--- src/test/scala/gerritforge/GerritGitSimulation.scala
+++ src/test/scala/gerritforge/GerritGitSimulation.scala
@@ -13,7 +13,7 @@ class GerritGitSimulation extends Simulation {
   val hostname    = InetAddress.getLocalHost.getHostName
   val gitProtocol = GitProtocol()
   val feeder = (1 to testConfig.numUsers) map { idx =>
-    Map("refSpec" -> s"branch-$hostname-$idx", "force" -> true)
+    Map("refSpec" -> s"branch-$hostname-$idx", "force" -> false)
   }
 
   val gitSshScenario  = testConfig.sshUrl.map(GerritGitScenario)
@@ -22,13 +22,13 @@ class GerritGitSimulation extends Simulation {
   val gitCloneAndPush: ScenarioBuilder = scenario("Git clone and push to Gerrit")
     .feed(feeder.circular)
     .doIf(gitSshScenario.isDefined) {
-      exec(gitSshScenario.get.pushCommand)
-        .exec(gitSshScenario.get.cloneCommand)
+        exec(gitSshScenario.get.cloneCommand)
+        .exec(gitSshScenario.get.pushCommand)
         .exec(gitSshScenario.get.createChangeCommand)
     }
     .doIf(gitHttpScenario.isDefined) {
-      exec(gitHttpScenario.get.pushCommand)
-        .exec(gitHttpScenario.get.cloneCommand)
+        exec(gitHttpScenario.get.cloneCommand)
+          .exec(gitHttpScenario.get.pushCommand)
         .exec(gitHttpScenario.get.createChangeCommand)
     }
 
