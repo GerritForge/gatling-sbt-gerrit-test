7bfd76905eda4aff8ec2772243d43a0856ba22d8
Daniele Sassoli <danielesassoli@gmail.com>
2023-03-20 08:36:18 +0000

    Add simulation for Abandon Change scenario
    Change-Id: I148dcbaca2c4c0b7fd912d883f0845affe85c09c

--- src/test/scala/gerritforge/GerritRestSimulation.scala
+++ src/test/scala/gerritforge/GerritRestSimulation.scala
@@ -1,32 +1,14 @@
 package gerritforge
 
-import gerritforge.ChangesListScenario._
-import gerritforge.GerritTestConfig._
-import gerritforge.restsimulations.GatlingRestUtils.httpProtocol
-import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
+import io.gatling.core.Predef._
+import GerritTestConfig._
+import ChangesListScenario._
+import gerritforge.restsimulations.AbandonChange
 import io.gatling.core.structure.ScenarioBuilder
-
-import scala.util.Random
+import gerritforge.restsimulations.GatlingRestUtils._
 
 class GerritRestSimulation extends Simulation {
-
-  val restApiHeader = Map(
-    "Accept"                    -> "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Pragma"                    -> "no-cache",
-    "Upgrade-Insecure-Requests" -> "1"
-  )
-
-  val randomInt = new Random
-
-  val randomReview = Iterator.continually(
-    Map(
-      "reviewMessage" -> (1 to (10 + randomInt.nextInt(10)))
-        .map(_ => Random.alphanumeric.take(randomInt.nextInt(10)).mkString)
-        .mkString(" "),
-      "reviewScore" -> (randomInt.nextInt(5) - 2)
-    )
-  )
 
   val anonymousUserChangeList = List(
     scenario("Anonymous user").exec(listChanges(testConfig.project))
@@ -38,7 +20,8 @@ class GerritRestSimulation extends Simulation {
   )
 
   val scenarios =
-    if (testConfig.restRunAnonymousUser) authenticatedChangeList ++ anonymousUserChangeList
+    if (testConfig.restRunAnonymousUser)
+      AbandonChange.scn :: authenticatedChangeList ++ anonymousUserChangeList
     else authenticatedChangeList
 
   require(httpProtocol.isDefined, "GERRIT_HTTP_URL must be defined to run REST-API simulation")
