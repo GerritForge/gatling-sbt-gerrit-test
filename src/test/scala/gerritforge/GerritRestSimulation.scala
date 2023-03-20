18c06fa31ac765374d1ed3842725e0d62d7cff6f
Daniele Sassoli <danielesassoli@gmail.com>
2023-03-20 08:56:37 +0000

    Extracting GerritRestSimulation methods to clean up main class
    Change-Id: Ib6bfcc431a0ce45e87d90dcf946d88ece94cdf4a

--- src/test/scala/gerritforge/GerritRestSimulation.scala
+++ src/test/scala/gerritforge/GerritRestSimulation.scala
@@ -1,48 +1,13 @@
 package gerritforge
 
-import io.gatling.core.scenario.Simulation
-
+import gerritforge.ChangesListScenario._
+import gerritforge.GerritTestConfig._
+import gerritforge.restsimulations.GatlingRestUtils.{httpProtocol, randomReview}
 import io.gatling.core.Predef._
-import io.gatling.http.Predef._
-import GerritTestConfig._
-import ChangesListScenario._
+import io.gatling.core.scenario.Simulation
 import io.gatling.core.structure.ScenarioBuilder
-import io.gatling.http.protocol.HttpProtocol
-
-import scala.util.Random
 
 class GerritRestSimulation extends Simulation {
-
-  val httpProtocol: Option[HttpProtocol] = testConfig.httpUrl.map(
-    url =>
-      http
-        .baseUrl(url)
-        .inferHtmlResources(
-          AllowList(),
-          DenyList(""".*\.js""", """.*\.css""", """.*\.ico""", """.*\.woff2""", """.*\.png""")
-        )
-        .acceptHeader("*/*")
-        .acceptEncodingHeader("gzip, deflate")
-        .acceptLanguageHeader("en-GB,en;q=0.5")
-        .userAgentHeader("gatling-test")
-  )
-
-  val restApiHeader = Map(
-    "Accept"                    -> "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Pragma"                    -> "no-cache",
-    "Upgrade-Insecure-Requests" -> "1"
-  )
-
-  val randomInt = new Random
-
-  val randomReview = Iterator.continually(
-    Map(
-      "reviewMessage" -> (1 to (10 + randomInt.nextInt(10)))
-        .map(_ => Random.alphanumeric.take(randomInt.nextInt(10)).mkString)
-        .mkString(" "),
-      "reviewScore" -> (randomInt.nextInt(5) - 2)
-    )
-  )
 
   val anonymousUserChangeList = List(
     scenario("Anonymous user").exec(listChanges(testConfig.project))
