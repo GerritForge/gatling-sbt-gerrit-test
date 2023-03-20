d8958f2f395bb3366a18348b30c6466690939a85
Daniele Sassoli <danielesassoli@gmail.com>
2023-03-20 11:14:51 +0000

    Adding Restore scenario to change simulations
    Change-Id: I5409d51db3ad6b9439a5fe7da73aa87238ef0333

--- src/test/scala/gerritforge/restsimulations/ChangeScenarios.scala
+++ src/test/scala/gerritforge/restsimulations/ChangeScenarios.scala
@@ -5,16 +5,25 @@ import gerritforge.restsimulations.GatlingRestUtils._
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
+
+import scala.concurrent.duration.DurationInt
 object ChangeScenarios extends ScenarioBase {
 
-  val abandonChangeScn: ScenarioBuilder = {
+  val abandonAndRestoreChangeScn: ScenarioBuilder =
     setupAuthenticatedSession("Abandon Change")
       .exec(
-        http("Abandon change")
+        http("Abandon Change")
           .post(s"/changes/${testConfig.project}~#{changeNumber}/abandon")
           .headers(postApiHeader(testConfig.xsrfToken))
           .body(StringBody("{}"))
       )
-  }
-  override val scns: List[ScenarioBuilder] = List(abandonChangeScn)
+      .pause(1 second)
+      .exec(
+        http("Restore change")
+          .post(s"/changes/${testConfig.project}~#{changeNumber}/restore")
+          .headers(postApiHeader(testConfig.xsrfToken))
+          .body(StringBody("{}"))
+      )
+
+  override val scns: List[ScenarioBuilder] = List(abandonAndRestoreChangeScn)
 }
