313a98ea599fc73ae6d71388bca0e752a99fa633
Daniele Sassoli <danielesassoli@gmail.com>
2023-03-21 19:37:36 +0000

    Add Make Change WIP scenario
    Change-Id: I7467f70633d189ea739051012b14fdb6d80ef5cd

--- src/test/scala/gerritforge/restsimulations/ChangeScenarios.scala
+++ src/test/scala/gerritforge/restsimulations/ChangeScenarios.scala
@@ -6,6 +6,8 @@ import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
 
+import java.util.Calendar
+
 object ChangeScenarios extends ScenarioBase {
 
   val abandonAndRestoreChangeScn: ScenarioBuilder =
@@ -25,5 +27,55 @@ object ChangeScenarios extends ScenarioBase {
           .body(StringBody("{}"))
       )
 
-  override val scns: List[ScenarioBuilder] = List(abandonAndRestoreChangeScn)
+  val submitChangeScn: ScenarioBuilder = {
+    setupAuthenticatedSession("Submit Change")
+      .exec(
+        http("Create Change")
+          .post("/changes/")
+          .headers(postApiHeader(testConfig.xsrfToken))
+          .body(StringBody(s"""{"project":"${testConfig.project}",
+            |"branch":"master",
+            |"subject":"Test commit subject - ${Calendar.getInstance().getTime}"}""".stripMargin))
+          .check(regex("_number\":(\\d+),").saveAs("newChangeNumber"))
+      )
+      .pause(1)
+      .exec(
+        http("Approve Change")
+          .post(s"/changes/${testConfig.project}~#{newChangeNumber}/revisions/1/review")
+          .headers(postApiHeader(testConfig.xsrfToken))
+          .body(StringBody("""{"labels":{"Code-Review":2}}"""))
+          .asJson
+      )
+      .pause(1)
+      .exec(
+        http("Submit Change")
+          .post(s"/changes/${testConfig.project}~#{newChangeNumber}/revisions/1/submit")
+          .headers(postApiHeader(testConfig.xsrfToken))
+          .body(StringBody("{}"))
+      )
+  }
+
+  val makeChangeWipScn: ScenarioBuilder =
+    setupAuthenticatedSession("Make change WIP")
+      .exec(
+        http("Create Change")
+          .post("/changes/")
+          .headers(postApiHeader(testConfig.xsrfToken))
+          .body(
+            StringBody(s"""{"project":"${testConfig.project}",
+               |"branch":"master",
+               |"subject":"Test commit subject - ${Calendar.getInstance().getTime}"}""".stripMargin)
+          )
+          .check(regex("_number\":(\\d+),").saveAs("changeToWip"))
+      )
+      .pause(1)
+      .exec(
+        http("Make Change WIP")
+          .post(s"/changes/${testConfig.project}~#{changeToWip}/wip")
+          .headers(postApiHeader(testConfig.xsrfToken))
+          .body(StringBody("""{}"""))
+      )
+
+  override val scns: List[ScenarioBuilder] =
+    List(abandonAndRestoreChangeScn, submitChangeScn, makeChangeWipScn)
 }
