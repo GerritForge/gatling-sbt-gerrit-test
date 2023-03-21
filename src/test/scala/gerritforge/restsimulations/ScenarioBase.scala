c547f4b782dc1d339b3cf9feaa29693964f117fc
Daniele Sassoli <danielesassoli@gmail.com>
2023-03-21 18:38:35 +0000

    Extract Logic to set the change number
    Change-Id: I3ef4b8077bdc62df8f5f77f57be2e4428d8138a2

--- src/test/scala/gerritforge/restsimulations/ScenarioBase.scala
+++ src/test/scala/gerritforge/restsimulations/ScenarioBase.scala
@@ -1,9 +1,6 @@
 package gerritforge.restsimulations
 
-import gerritforge.ChangeDetail
-import gerritforge.ChangesListScenario.randomNumber
 import gerritforge.GerritTestConfig.testConfig
-import gerritforge.restsimulations.GatlingRestUtils.firstOpenChangeDetails
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
@@ -19,17 +16,6 @@ trait ScenarioBase {
       case Some(cookie) =>
         scenario(scnTitle)
           .exec(addCookie(Cookie("GerritAccount", cookie)))
-          .exec(firstOpenChangeDetails(testConfig.project))
-          .doIf(session => session("changeDetails").as[List[ChangeDetail]].nonEmpty) {
-            exec { session =>
-              val changes: Seq[ChangeDetail] = session("changeDetails").as[List[ChangeDetail]]
-              val change                     = changes(randomNumber.nextInt(changes.size))
-              session.set(
-                "changeNumber",
-                change._number
-              )
-            }
-          }
       case None => throw new Exception("Requires authentication")
     }
   }
