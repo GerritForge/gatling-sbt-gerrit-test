3c7f15cb9828c3e763e61174a4283fc0269915ea
Marcin Czech <maczech@gmail.com>
2023-03-22 23:49:24 +0100

    Avoid multiple users removing the same tags
    Change-Id: I3decc9e0841f319e2763daefecafbbaeba68453e

--- src/test/scala/gerritforge/restsimulations/TagScenarios.scala
+++ src/test/scala/gerritforge/restsimulations/TagScenarios.scala
@@ -38,7 +38,7 @@ object TagScenarios extends ScenarioBase {
     setupAuthenticatedSession("List and remove a Tag")
       .exec(
         http("list tags")
-          .get(s"/projects/${testConfig.project}/tags/?n=${numTags}&S=0")
+          .get(s"/projects/${testConfig.project}/tags/?n=150")
           .headers(postApiHeader(testConfig.xsrfToken))
           .check(
             bodyString
@@ -51,13 +51,10 @@ object TagScenarios extends ScenarioBase {
               .saveAs("tagDetails")
           )
       )
-      .doIf(session => !session("tagDetails").as[List[TagDetail]].isEmpty) {
+      .doIf(session => session("tagDetails").as[List[TagDetail]].nonEmpty) {
         exec { session =>
-          val tags    = session("tagDetails").as[List[TagDetail]]
-          val numTags = randomNumTags.nextInt(tags.size)
-          val randomTagRefs = Random
-            .shuffle(tags)
-            .drop(numTags)
+          val tags = session("tagDetails").as[List[TagDetail]]
+          val randomTagRefs = tags
             .map(_.ref)
             .map(_.drop("refs/tags/".length))
           val tagNames = randomTagRefs.asJson
