978c6f48536ad5b001917aca3a828e00254c1590
Daniele Sassoli <danielesassoli@gmail.com>
2023-03-22 13:36:14 +0000

    Implement Add Reviewer scenario
    Change-Id: I1a116d31f7dffdad675bccb0d95ae52f3eaf8246

--- src/test/scala/gerritforge/restsimulations/ChangeScenarios.scala
+++ src/test/scala/gerritforge/restsimulations/ChangeScenarios.scala
@@ -75,6 +75,37 @@ object ChangeScenarios extends ScenarioBase {
           .body(StringBody("""{}"""))
       )
 
+  val addThenRemoveReviewerScn =
+    setupAuthenticatedSession("Add and Remove Reviewer")
+      .exec(
+        http("Get Account Details")
+          .get("/accounts/self")
+          .headers(postApiHeader(testConfig.xsrfToken))
+          .check(regex("_account_id\":(\\d+),").saveAs("selfAccountId"))
+      )
+      .exec(
+        http("Create Change")
+          .post("/changes/")
+          .headers(postApiHeader(testConfig.xsrfToken))
+          .body(
+            StringBody(s"""{"project":"${testConfig.project}",
+                 |"branch":"master",
+                 |"subject":"Test commit subject - ${Calendar
+                            .getInstance()
+                            .getTime}"}""".stripMargin)
+          )
+          .check(regex("_number\":(\\d+),").saveAs("changeToReview"))
+      )
+      .pause(1)
+      .exec(
+        http("Review Change")
+          .post(
+            s"/changes/${testConfig.project}~#{changeToReview}/revisions/1/review"
+          )
+          .headers(postApiHeader(testConfig.xsrfToken))
+          .body(StringBody(s"""{"reviewers":[{"reviewer":#{selfAccountId}}]}"""))
+      )
+
   override val scns: List[ScenarioBuilder] =
-    List(abandonAndRestoreChangeScn, submitChangeScn, makeChangeWipScn)
+    List(abandonAndRestoreChangeScn, submitChangeScn, makeChangeWipScn, addThenRemoveReviewerScn)
 }
