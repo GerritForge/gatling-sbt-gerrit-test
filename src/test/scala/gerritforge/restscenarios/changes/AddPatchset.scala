ab3683f222082ab4f57368d9dcb26409184187de
Daniele Sassoli <danielesassoli@gmail.com>
2023-03-28 17:41:33 +0100

    Add Patchset via RestAPI
    Change-Id: I2ada04d5369b83be2c3993d86b562039039d2bd3

---
+++ src/test/scala/gerritforge/restscenarios/changes/AddPatchset.scala
@@ -0,0 +1,30 @@
+package gerritforge.restscenarios.changes
+
+import gerritforge.GerritTestConfig.testConfig
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+object AddPatchset extends ChangeScenarioBase {
+  override def simulationName: String = "ADD_PATCHSET"
+
+  override def scn: ScenarioBuilder =
+    setupAuthenticatedSession("Add Patchset")
+      .exec(
+        createChange
+          .check(regex("_number\":(\\d+),").saveAs("changeNumber"))
+      )
+      .pause(pauseDuration, pauseStdDev)
+      .exec(
+        http("Add Patchset")
+          .put(s"/changes/${testConfig.encodedProject}~#{changeNumber}/edit/test-patchset.txt")
+          .headers(postApiHeader(testConfig.xsrfToken))
+          .body(StringBody("""{"binary_content":"data:text/plain;base64,c29tZSB0ZXN0Cg=="}"""))
+      )
+      .exec(
+        http("Publish Patchset")
+          .post(s"/changes/${testConfig.encodedProject}~#{changeNumber}/edit:publish")
+          .headers(postApiHeader(testConfig.xsrfToken))
+          .body(StringBody("""{"notify":"NONE"}"""))
+      )
+}
