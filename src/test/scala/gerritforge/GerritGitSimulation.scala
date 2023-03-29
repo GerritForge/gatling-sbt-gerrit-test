c835070d7721f5d79aca08562b2512f769c10396
Daniele Sassoli <danielesassoli@gmail.com>
2023-03-29 19:46:43 +0100

    Add hashtag for both userId and scenario when creating change
    This will allow rest scenarios to filter based on scenario name and user
    id, in order to avoid 409 conflicts when doing operation on changes.
    
    This is why the git create change scenario tags a list of rest scenario
    names.
    
    Change-Id: I716a26ff4866a021044d30a090608e18b3d91365

--- src/test/scala/gerritforge/GerritGitSimulation.scala
+++ src/test/scala/gerritforge/GerritGitSimulation.scala
@@ -2,11 +2,10 @@ package gerritforge
 
 import com.github.barbasa.gatling.git.protocol.GitProtocol
 import gerritforge.GerritTestConfig.testConfig
-import gerritforge.scenarios.git.{CloneCommand, CreateChangeCommand, PushCommand}
+import gerritforge.scenarios.git.{Clone, CreateChangeCommand, Push}
 import io.gatling.core.Predef._
-import io.gatling.core.scenario.Simulation
 
-class GerritGitSimulation extends Simulation {
+class GerritGitSimulation extends SimulationBase {
 
   val gitProtocol = GitProtocol()
 
@@ -14,9 +13,10 @@ class GerritGitSimulation extends Simulation {
     .flatMap(
       url =>
         List(
-          CloneCommand(url).scn,
-          CreateChangeCommand(url).scn,
-          PushCommand(url).scn
+          Clone(url).scn,
+          CreateChangeCommand(url, authenticatedScenarios.map(_.scenarioName)).scn,
+          Push(url).scn
+          //TODO Add forcePushScenario
         )
     )
     .toList
---
+++ src/test/scala/gerritforge/scenarios/git/CreateChange.scala
@@ -0,0 +1,41 @@
+package gerritforge.scenarios.git
+
+import com.github.barbasa.gatling.git.GitRequestSession
+import com.github.barbasa.gatling.git.request.builder.GitRequestBuilder
+import gerritforge.GerritTestConfig._
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+
+case class CreateChangeCommand(url: String, scenarioHashtags: Seq[String]) extends GitScenarioBase {
+
+  val hashtagLoop = scenarioHashtags.to(LazyList).lazyAppendedAll(scenarioHashtags)
+
+  override def scn: ScenarioBuilder =
+    scenario(s"Create Change Command over $protocol")
+      .feed(feeder.circular)
+      .feed(userIdFeeder)
+      .foreach(hashtagLoop, "hashtagId") {
+        exec(
+          new GitRequestBuilder(
+            GitRequestSession(
+              "push",
+              s"$url/${testConfig.encodedProject}",
+              "#{refSpec}"
+            )
+          )
+        ).pause(pauseDuration, pauseStdDev)
+          .exec(
+            new GitRequestBuilder(
+              GitRequestSession(
+                "push",
+                s"$url/${testConfig.encodedProject}",
+                "HEAD:refs/for/#{refSpec}",
+                computeChangeId = true,
+                pushOptions = "t=#{hashtagId},t=#{userId}"
+              )
+            )
+          )
+          .pause(pauseDuration, pauseStdDev)
+      }
+
+}
