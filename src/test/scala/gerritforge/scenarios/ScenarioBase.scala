161d25f945d682173616ea2671bf5bfee8e3d61d
Daniele Sassoli <danielesassoli@gmail.com>
2023-03-29 19:46:43 +0100

    Add hashtag for both userId and scenario when creating change
    This will allow rest scenarios to filter based on scenario name and user
    id, in order to avoid 409 conflicts when doing operation on changes.
    
    Bug: Issue 16891
    Change-Id: I716a26ff4866a021044d30a090608e18b3d91365

--- src/test/scala/gerritforge/scenarios/ScenarioBase.scala
+++ src/test/scala/gerritforge/scenarios/ScenarioBase.scala
@@ -1,9 +1,11 @@
 package gerritforge.scenarios
 
+import gerritforge.GerritTestConfig.testConfig
 import gerritforge.PauseSimulation
 import io.gatling.core.structure.ScenarioBuilder
 
 trait ScenarioBase extends PauseSimulation {
   def scn: ScenarioBuilder
 
+  val userIdFeeder = (1 to testConfig.numUsers).map(userId => Map("userId" -> s"user-$userId"))
 }
