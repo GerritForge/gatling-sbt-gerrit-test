2af56b3178592b065d6b12806ba5d64b182cfe3a
Daniele Sassoli <danielesassoli@gmail.com>
2023-03-29 14:10:12 +0100

    Refactor GerritGitSimulation to resemble the Rest setup
    This also involved renaming the package for the REST simulations to
    improve separation of concerns.
    
    Bug: Issue 16891
    Change-Id: I5447f1f9f2ad3fa6aa48e7dcbc2a94fc730e6070

---
+++ src/test/scala/gerritforge/scenarios/git/GitScenarioBase.scala
@@ -0,0 +1,20 @@
+package gerritforge.scenarios.git
+
+import com.github.barbasa.gatling.git.GatlingGitConfiguration
+import gerritforge.GerritTestConfig.testConfig
+import gerritforge.scenarios.ScenarioBase
+
+import java.net.InetAddress
+
+trait GitScenarioBase extends ScenarioBase {
+
+  val url: String
+  val protocol = url.split(":").head
+
+  val hostname           = InetAddress.getLocalHost.getHostName
+  implicit val gitConfig = GatlingGitConfiguration()
+
+  val feeder = (1 to testConfig.numUsers) map { idx =>
+    Map("refSpec" -> s"branch-$hostname-$idx", "force" -> true)
+  }
+}
