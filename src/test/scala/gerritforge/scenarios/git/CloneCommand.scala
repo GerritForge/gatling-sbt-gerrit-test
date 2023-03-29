a30f4c27347098a369d920c86ad710a51ee5d220
Daniele Sassoli <danielesassoli@gmail.com>
2023-03-29 14:10:12 +0100

    Refactor GerritGitSimulation to resemble the Rest setup
    Change-Id: I5447f1f9f2ad3fa6aa48e7dcbc2a94fc730e6070

---
+++ src/test/scala/gerritforge/scenarios/git/CloneCommand.scala
@@ -0,0 +1,23 @@
+package gerritforge.scenarios.git
+
+import com.github.barbasa.gatling.git.GitRequestSession
+import com.github.barbasa.gatling.git.request.builder.GitRequestBuilder
+import gerritforge.GerritTestConfig._
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+
+case class CloneCommand(url: String) extends GitScenarioBase {
+  override def scn: ScenarioBuilder =
+    scenario(s"Clone Command over $protocol")
+      .feed(feeder.circular)
+      .exec(
+        new GitRequestBuilder(
+          GitRequestSession(
+            "clone",
+            s"$url/${testConfig.encodedProject}",
+            "#{refSpec}",
+            ignoreFailureRegexps = List(".*want.+not valid.*")
+          )
+        )
+      )
+}
