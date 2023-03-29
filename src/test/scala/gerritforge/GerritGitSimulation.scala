7c8d369f7c8be9b47af31f8cbcc198ed5fd87894
Daniele Sassoli <danielesassoli@gmail.com>
2023-03-29 14:10:12 +0100

    Refactor GerritGitSimulation to resemble the Rest setup
    Change-Id: I5447f1f9f2ad3fa6aa48e7dcbc2a94fc730e6070

--- src/test/scala/gerritforge/GerritGitSimulation.scala
+++ src/test/scala/gerritforge/GerritGitSimulation.scala
@@ -2,44 +2,30 @@ package gerritforge
 
 import com.github.barbasa.gatling.git.protocol.GitProtocol
 import gerritforge.GerritTestConfig.testConfig
+import gerritforge.scenarios.git.{CloneCommand, CreateChangeCommand, PushCommand}
 import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
-import io.gatling.core.structure.ScenarioBuilder
-
-import java.net.InetAddress
 
 class GerritGitSimulation extends Simulation {
 
-  val hostname    = InetAddress.getLocalHost.getHostName
   val gitProtocol = GitProtocol()
-  val feeder = (1 to testConfig.numUsers) map { idx =>
-    Map("refSpec" -> s"branch-$hostname-$idx", "force" -> true)
-  }
 
-  val gitSshScenario  = testConfig.sshUrl.map(GerritGitScenario)
-  val gitHttpScenario = testConfig.httpUrl.map(_ + "/a").map(GerritGitScenario)
-
-  val gitCloneAndPush: ScenarioBuilder = scenario("Git clone and push to Gerrit")
-    .feed(feeder.circular)
-    .doIf(gitSshScenario.isDefined) {
-      exec(gitSshScenario.get.pushCommand)
-        .exec(gitSshScenario.get.cloneCommand)
-        .exec(gitSshScenario.get.createChangeCommand)
-    }
-    .doIf(gitHttpScenario.isDefined) {
-      exec(gitHttpScenario.get.pushCommand)
-        .exec(gitHttpScenario.get.cloneCommand)
-        .exec(gitHttpScenario.get.createChangeCommand)
-    }
-
-  require(
-    testConfig.httpUrl.orElse(testConfig.sshUrl).isDefined,
-    "Either GERRIT_HTTP_URL or GERRIT_SSH_URL must be defined"
-  )
+  val scenarios = (testConfig.sshUrl ++ testConfig.httpUrl)
+    .flatMap(
+      url =>
+        List(
+          CloneCommand(url).scn,
+          CreateChangeCommand(url).scn,
+          PushCommand(url).scn
+        )
+    )
+    .toList
 
   setUp(
-    gitCloneAndPush.inject(
-      rampConcurrentUsers(1) to testConfig.numUsers during (testConfig.duration)
+    scenarios.map(
+      _.inject(
+        rampConcurrentUsers(1) to testConfig.numUsers during (testConfig.duration)
+      )
     )
   ).protocols(gitProtocol)
 }
