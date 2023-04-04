e6432ebb1e5b47b91f81aa648310e46099ca4c56
Daniele Sassoli <danielesassoli@gmail.com>
2023-04-04 21:49:59 +0100

    Specify name for git requests
    Change-Id: I01315ceb873e56ec74bb0098a7fc41a531f8ac23

--- src/test/scala/gerritforge/GerritRestSimulation.scala
+++ src/test/scala/gerritforge/GerritRestSimulation.scala
@@ -1,26 +1,26 @@
-package gerritforge
-
-import gerritforge.GerritTestConfig._
-import gerritforge.scenarios.rest.tags.CreateTag.httpProtocol
-import io.gatling.core.Predef._
-
-import scala.concurrent.duration.FiniteDuration
-
-class GerritRestSimulation extends SimulationBase {
-
-  val scenarios =
-    if (testConfig.restRunAnonymousUser)
-      allRestScenarios
-    else authenticatedScenarios
-
-  require(httpProtocol.isDefined, "GERRIT_HTTP_URL must be defined to run REST-API simulation")
-
-  val pauseStdDevSecs = 5
-  setUp(
-    scenarios.toList.map(
-      _.scn
-        .inject(rampConcurrentUsers(1) to testConfig.numUsers during testConfig.duration)
-        .pauses(normalPausesWithStdDevDuration(FiniteDuration(pauseStdDevSecs, "seconds")))
-    )
-  ).protocols(httpProtocol)
-}
+//package gerritforge
+//
+//import gerritforge.GerritTestConfig._
+//import gerritforge.scenarios.rest.tags.CreateTag.httpProtocol
+//import io.gatling.core.Predef._
+//
+//import scala.concurrent.duration.FiniteDuration
+//
+//class GerritRestSimulation extends SimulationBase {
+//
+//  val scenarios =
+//    if (testConfig.restRunAnonymousUser)
+//      allRestScenarios
+//    else authenticatedScenarios
+//
+//  require(httpProtocol.isDefined, "GERRIT_HTTP_URL must be defined to run REST-API simulation")
+//
+//  val pauseStdDevSecs = 5
+//  setUp(
+//    scenarios.toList.map(
+//      _.scn
+//        .inject(rampConcurrentUsers(1) to testConfig.numUsers during testConfig.duration)
+//        .pauses(normalPausesWithStdDevDuration(FiniteDuration(pauseStdDevSecs, "seconds")))
+//    )
+//  ).protocols(httpProtocol)
+//}
