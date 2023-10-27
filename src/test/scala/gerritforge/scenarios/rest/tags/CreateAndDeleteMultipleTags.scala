5b2c8f2fb0034fec7f529ee4519e34843c99e59c
Christoforos Miliotis <christoforos.miliotis.dev@gmail.com>
2023-10-27 15:38:27 +0100

    Add delete multiple tags scenario
    As part of addressing 409 failures in [1], the testing of the bulk tag
    deletion endpoint was removed. In the past this was problematic and
    the source of some of the 409 failures. This is because the creation
    of the tags was happening in a different scenario to the deletion of
    the tags. Regardless, this is an endpoint we would like to test with
    gatling.
    
    Create the tags in the same scenario as the bulk-deletion. There are
    essentially 3 operations in the scenario now:
    * the creation of a tag with a well-defined tag name which includes an
    `scnmultipletags` prefix, the user id and the current timestamp. The
    prefix effectively "isolates" the tags to the scenario responsible for
    their creation and helps avoid clashes with tags created by other
    scenarios. The combination of prefix, user id and timestamp should
    guarantee unique tag names.
    * the listing of the tags for a particular user, similar to [1]
    * the bulk-deletion of the tags for a particular user, using the POST
    endpoint, provided the number of tags for the user is higher to an
    arbitrary threshold. Again the logic here is fairly similar to [1].
    
    [1] https://review.gerrithub.io/c/GerritForge/gatling-sbt-gerrit-test/+/1170812
    
    Bug: Issue 306115162
    Change-Id: I289d8979dd935f2ba4aa4bc9097d2d2b17a95a11

---
+++ src/test/scala/gerritforge/scenarios/rest/tags/CreateAndDeleteMultipleTags.scala
@@ -0,0 +1,70 @@
+package gerritforge.scenarios.rest.tags
+
+import gerritforge.GerritTestConfig.testConfig
+import gerritforge.scenarios.rest.RestScenarioBase
+import io.circe.generic.auto._
+import io.circe.parser.decode
+import io.circe.syntax._
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+object CreateAndDeleteMultipleTags extends RestScenarioBase {
+
+  private def currentTimestamp: Long = System.currentTimeMillis()
+  private val tagsToDeleteAtOnce = 3
+
+  case class TagDetail(ref: String, revision: String)
+
+  override val scn: ScenarioBuilder =
+    setupAuthenticatedSession("Create and Delete Multiple Tags")
+      .feed(userIdFeeder.circular)
+      .exec { session =>
+        val userId = session("userId").as[String]
+        val tagId = s"scnmultipletags-$userId-$currentTimestamp"
+        session.set("latestTagId", tagId)
+      }
+      .exec(
+        http("create multiple tags")
+          .put(s"/projects/${testConfig.encodedProject}/tags/#{latestTagId}")
+          .headers(addApiHeaders(testConfig.xsrfToken))
+          .body(StringBody("""{"revision":"HEAD"}"""))
+          .asJson
+      )
+      .pause(pauseDuration, pauseStdDev)
+      .exec(
+        http("list tags")
+          .get(
+            s"/projects/${testConfig.encodedProject}/tags/?m=scnmultipletags-#{userId}-"
+          )
+          .headers(addApiHeaders(testConfig.xsrfToken))
+          .check(
+            bodyString
+              .transform(_.drop(XSS_LEN))
+              .transform(decode[List[TagDetail]](_))
+              .transform {
+                case Right(tagDetailList) => tagDetailList
+                case Left(decodingError) => throw decodingError
+              }
+              .saveAs("tagDetails")
+          )
+      )
+      .doIf(session => session("tagDetails").as[List[TagDetail]].size >= tagsToDeleteAtOnce) {
+        exec { session =>
+          val allTagsCreatedSoFar =
+            session("tagDetails")
+              .as[List[TagDetail]]
+              .map(_.ref)
+              .map(_.stripPrefix("refs/tags/"))
+              .asJson
+          session.set("tagNames", allTagsCreatedSoFar)
+        }.exec(
+            http("delete multiple tags at once")
+              .post(s"/projects/${testConfig.encodedProject}/tags:delete")
+              .headers(addApiHeaders(testConfig.xsrfToken))
+              .body(StringBody(s"""{"tags": #{tagNames}}"""))
+              .asJson
+        )
+        .pause(pauseDuration, pauseStdDev)
+      }
+}
