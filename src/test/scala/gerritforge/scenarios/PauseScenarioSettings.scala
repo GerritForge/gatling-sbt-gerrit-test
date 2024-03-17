075f7848a18f4a1232f9ffe8e84421792e231315
Fabio Ponciroli <ponch78@gmail.com>
2024-03-17 17:16:03 +0000

    Allow random generator seed injection
    Currently the standard deviation is randomly
    generated.
    
    The Gatling `normalPausesWithStdDevDuration` method
    doesn't expose any API to inject the random
    generator seed.
    
    This prevents the repeatability of a scenario.
    
    Use Custom PauseType [1] and "manually" calculate
    the random standard deviation.
    
    [1]: https://github.com/gatling/gatling/blob/main/gatling-core-java/src/main/java/io/gatling/javaapi/core/PauseType.java#L115
    
    Change-Id: Ia8840875d93c4dce730e5638205ad724c4b7ded8

--- src/test/scala/gerritforge/scenarios/PauseScenarioSettings.scala
+++ src/test/scala/gerritforge/scenarios/PauseScenarioSettings.scala
@@ -1,8 +1,7 @@
 package gerritforge.scenarios
 
-import io.gatling.core.Predef.normalPausesWithStdDevDuration
-
-import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration.{Duration, FiniteDuration}
+import scala.util.Random
 
 trait PauseScenarioSettings {
   def scenarioName: String = {
@@ -12,12 +11,19 @@ trait PauseScenarioSettings {
     }
   }
 
+  lazy val random: Random = {
+    val seed: Option[Long] = sys.env.get("STDDEV_SEED").map(_.toLong)
+    seed.fold(new Random())(new Random(_))
+  }
+
   lazy val pauseDuration: FiniteDuration =
     FiniteDuration(sys.env.getOrElse(s"${scenarioName}_PAUSE", "0").toLong, "seconds")
   lazy val stdDevDuration =
     FiniteDuration(sys.env.getOrElse(s"${scenarioName}_STDDEV_PAUSE", "0").toLong, "seconds")
-  lazy val pauseStdDev = normalPausesWithStdDevDuration(stdDevDuration)
   println(
     s"$scenarioName: sleeping for ${pauseDuration.toMillis} ms with a ${stdDevDuration.toMillis}ms stdandard deviation"
   )
+  lazy val pauseStdDev: Duration = {
+    random.nextGaussian() * stdDevDuration
+  }
 }
