4f850a558777c3eefad65c9df6542d5ffeab8b58
Daniele Sassoli <danielesassoli@gmail.com>
2024-04-22 21:55:34 +0200

    Create BitBucket backend
    Change-Id: Id0622f22e0041ea4903652114919f6763253c4dc

--- src/test/scala/gerritforge/scenarios/git/CloneCommand.scala
+++ src/test/scala/gerritforge/scenarios/git/CloneCommand.scala
@@ -3,22 +3,21 @@ package gerritforge.scenarios.git
 import com.github.barbasa.gatling.git.GitRequestSession
 import com.github.barbasa.gatling.git.GitRequestSession.MasterRef
 import com.github.barbasa.gatling.git.request.builder.GitRequestBuilder
-
 import gerritforge.config.SimulationConfig.simulationConfig
+import gerritforge.scenarios.git.backend.GitServer
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 
-class CloneCommand(val url: String) extends GitScenarioBase {
+class CloneCommand(val gitServer: GitServer, val url: String) extends GitScenarioBase {
 
   override def scn: ScenarioBuilder =
     scenario(s"Clone Command over $protocol")
       .feed(userIdFeeder.circular)
-      .pause(pauseDuration, pauseStdDev)
       .exec(
         new GitRequestBuilder(
           GitRequestSession(
             "clone",
-            s"$url/${simulationConfig.project}",
+            s"${gitServer.baseHttpUrl(url)}/${simulationConfig.project}${gitServer.httpUrlSuffix}",
             MasterRef,
             ignoreFailureRegexps = List(".*want.+not valid.*"),
             requestName = s"Clone over $protocol"
