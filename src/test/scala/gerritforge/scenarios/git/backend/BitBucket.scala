42bd39e3cb483bde9ca7825fccbe38d6d7c4aa1f
Daniele Sassoli <danielesassoli@gmail.com>
2024-04-22 21:55:34 +0200

    Create BitBucket backend
    Change-Id: Id0622f22e0041ea4903652114919f6763253c4dc

---
+++ src/test/scala/gerritforge/scenarios/git/backend/BitBucket.scala
@@ -0,0 +1,91 @@
+package gerritforge.scenarios.git.backend
+import com.github.barbasa.gatling.git.GitRequestSession.MasterRef
+import com.github.barbasa.gatling.git.{GatlingGitConfiguration, GitRequestSession}
+import com.github.barbasa.gatling.git.request.builder.GitRequestBuilder
+import io.gatling.core.Predef._
+import io.gatling.http.Predef.http
+
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+import scala.concurrent.duration.DurationInt
+import scala.util.Random
+
+case class BitBucket(username: String, password: String, repository: String) extends GitServer {
+
+  def apiBaseUrl(projKey: String = "LOAD") = s"rest/api/latest/projects/$projKey/repos/$repository"
+  val basicAuthValue =
+    Base64.getEncoder
+      .encodeToString(s"$username:$password".getBytes(StandardCharsets.UTF_8))
+
+  override val refSpecFeeder: Iterator[Map[String, String]] =
+    Iterator
+      .continually(
+        Map("refSpec" -> s"branch-${Random.nextInt(9999)}")
+      )
+
+  override def createChange(
+      origin: String,
+      refSpec: String,
+      userId: String,
+      protocol: String,
+      hashtags: Seq[String]
+  )(
+      implicit conf: GatlingGitConfiguration
+  ) = {
+    exec(
+      new GitRequestBuilder(
+        GitRequestSession(
+          "push",
+          s"${baseHttpUrl(origin)}/$repository$httpUrlSuffix",
+          refSpec,
+          userId = "#{userId}",
+          requestName = s"Create branch over $protocol",
+          repoDirOverride = "/tmp/#{userId}"
+        )
+      )
+    ).pause(500.milliseconds)
+      .exec(
+        http("Create Change over http")
+          .post(s"$origin/${apiBaseUrl()}/pull-requests")
+          .headers(
+            Map(
+              "Authorization" -> s"Basic $basicAuthValue",
+              "Content-Type"  -> "application/json"
+            )
+          )
+          .body(StringBody(s"""{
+                          |    "title": "Change from branch $refSpec",
+                          |    "description": "Test description from branch $refSpec",
+                          |    "state": "OPEN",
+                          |    "open": true,
+                          |    "closed": false,
+                          |    "fromRef": {
+                          |        "id": "$refSpec",
+                          |        "repository": {
+                          |            "slug": "load-test",
+                          |            "name": null,
+                          |            "project": {
+                          |                "key": "LOAD"
+                          |            }
+                          |        }
+                          |    },
+                          |    "toRef": {
+                          |        "id": "$MasterRef",
+                          |        "repository": {
+                          |            "slug": "load-test",
+                          |            "name": null,
+                          |            "project": {
+                          |                "key": "LOAD"
+                          |            }
+                          |        }
+                          |    },
+                          |    "locked": false,
+                          |    "reviewers": []
+                          |}""".stripMargin))
+      )
+  }
+
+  override def baseHttpUrl(url: String): String = url + "/scm/load"
+
+  override val httpUrlSuffix: String = ".git"
+}
