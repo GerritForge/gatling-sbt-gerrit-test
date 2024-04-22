ab1518d072975acb7f9c97340ad678d48786e36f
Daniele Sassoli <danielesassoli@gmail.com>
2024-04-22 21:55:34 +0200

    Create BitBucket backend
    Change-Id: Id0622f22e0041ea4903652114919f6763253c4dc

--- src/test/scala/gerritforge/config/SimulationConfig.scala
+++ src/test/scala/gerritforge/config/SimulationConfig.scala
@@ -1,10 +1,11 @@
 package gerritforge.config
 
-import gerritforge.scenarios.git.backend.Gerrit
+import gerritforge.scenarios.git.backend.{BitBucket, Gerrit}
 import pureconfig.ConfigSource
 import pureconfig.generic.auto._
 import scala.concurrent.duration.FiniteDuration
 import gerritforge.EncodeUtils.encode
+import HttpConfig.httpConfig
 
 object SimulationConfig {
   val simulationConfig = ConfigSource.default.at("simulation").loadOrThrow[SimulationConfig]
@@ -15,8 +16,12 @@ case class SimulationConfig(
     sshUrl: Option[String],
     project: String,
     numUsers: Int,
-    duration: FiniteDuration
+    duration: FiniteDuration,
+    backend: String
 ) {
   val encodedProject = encode(project)
-  val gitBackend     = Gerrit
+  val gitBackend = backend.toLowerCase() match {
+    case "bitbucket" => BitBucket(httpConfig.username, httpConfig.password, encodedProject)
+    case _           => Gerrit(encodedProject, numUsers)
+  }
 }
