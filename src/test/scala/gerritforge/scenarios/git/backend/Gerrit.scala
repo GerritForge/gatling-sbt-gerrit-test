2a9705940a713b621bbd6149e664f28a8a6572db
Daniele Sassoli <danielesassoli@gmail.com>
2024-05-07 18:05:45 +0100

    Move creating a change per hashtag within Gerrit backend
    Change-Id: I7eed5319ff1b63e5928ab7775026873bcf20cbad

--- src/test/scala/gerritforge/scenarios/git/backend/Gerrit.scala
+++ src/test/scala/gerritforge/scenarios/git/backend/Gerrit.scala
@@ -1,25 +1,36 @@
 package gerritforge.scenarios.git.backend
 
-import com.github.barbasa.gatling.git.{GatlingGitConfiguration, GitRequestSession}
 import com.github.barbasa.gatling.git.request.builder.GitRequestBuilder
+import com.github.barbasa.gatling.git.{GatlingGitConfiguration, GitRequestSession}
+import com.github.barbasa.gatling.git.request.builder.GitRequestBuilder.toActionBuilder
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
 
 object Gerrit extends GitServer {
 
-  override def createChange(origin: String, ref: String, userId: String, protocol: String)(
-      implicit
-      conf: GatlingGitConfiguration
-  ): GitRequestBuilder = {
-    new GitRequestBuilder(
-      GitRequestSession(
-        "push",
-        origin,
-        s"HEAD:refs/for/$ref",
-        computeChangeId = true,
-        pushOptions = s"hashtag=#{hashtagId},hashtag=#{userId}",
-        userId = userId,
-        requestName = s"Push to new branch over $protocol"
+  override def createChange(
+      origin: String,
+      ref: String,
+      userId: String,
+      protocol: String,
+      hashtags: Seq[String]
+  )(
+      implicit conf: GatlingGitConfiguration
+  ): ChainBuilder = {
+    new ChainBuilder(hashtags.map { hashtag =>
+      toActionBuilder(
+        new GitRequestBuilder(
+          GitRequestSession(
+            "push",
+            origin,
+            s"HEAD:refs/for/$ref",
+            computeChangeId = true,
+            pushOptions = s"hashtag=$hashtag,hashtag=#{userId}",
+            userId = userId,
+            requestName = s"Push to new branch over $protocol"
+          )
+        )
       )
-    )
+    }.toList)
   }
 }
