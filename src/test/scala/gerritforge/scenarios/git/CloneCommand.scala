50da577a623e13347e90fc96df68a226b57666f1
Fabio Ponciroli <ponch78@gmail.com>
2024-05-08 15:54:59 +0200

    Always clone from master
    There is no need of creating new branches when
    cloning. We can always clone from the master branch.
    
    Change-Id: I19a7ae96c491fd17977a6159462b25628fd57d68

--- src/test/scala/gerritforge/scenarios/git/CloneCommand.scala
+++ src/test/scala/gerritforge/scenarios/git/CloneCommand.scala
@@ -1,6 +1,7 @@
 package gerritforge.scenarios.git
 
 import com.github.barbasa.gatling.git.GitRequestSession
+import com.github.barbasa.gatling.git.GitRequestSession.MasterRef
 import com.github.barbasa.gatling.git.request.builder.GitRequestBuilder
 import gerritforge.GerritTestConfig._
 import io.gatling.core.Predef._
@@ -10,33 +11,14 @@ class CloneCommand(val url: String) extends GitScenarioBase {
 
   override def scn: ScenarioBuilder =
     scenario(s"Clone Command over $protocol")
-      .feed(refSpecFeeder.circular)
       .feed(userIdFeeder.circular)
-      .doIf { session =>
-        !alreadyFedUsers.contains(session("userId").as[String])
-      } {
-        exec { session =>
-          alreadyFedUsers = session("userId").as[String] :: alreadyFedUsers
-          session
-        }.exec(
-          new GitRequestBuilder(
-            //We push a new refspec as the subsequent clone needs the ref to exist.
-            GitRequestSession(
-              "push",
-              s"$url/${testConfig.project}",
-              s"#{refSpec}",
-              requestName = s"Push to setup Clone over $protocol"
-            )
-          )
-        )
-      }
       .pause(pauseDuration, pauseStdDev)
       .exec(
         new GitRequestBuilder(
           GitRequestSession(
             "clone",
             s"$url/${testConfig.project}",
-            "#{refSpec}",
+            MasterRef,
             ignoreFailureRegexps = List(".*want.+not valid.*"),
             requestName = s"Clone over $protocol"
           )
