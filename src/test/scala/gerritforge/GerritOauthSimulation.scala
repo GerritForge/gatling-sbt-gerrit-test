38e91960b25cc89741d979a632c98b713380c4f3
Diego Zambelli Sessona <diego.sessona@gmail.com>
2024-06-27 22:02:51 +0200

    Add a simulation to test Gerrit oauth plugin
    The simulation GerritOauthSimulation adds functional tests
    to check that user can or cannot login into Gerrit based
    on the user existance and validity of its credentials in
    the oauth provider.
    
    Change-Id: I62f8e8c6b807ff174a8f890b3f21a6b2f2c3a790

---
+++ src/test/scala/gerritforge/GerritOauthSimulation.scala
@@ -0,0 +1,33 @@
+package gerritforge
+
+import gerritforge.config.SimulationConfig.simulationConfig
+import gerritforge.scenarios.oauth.{InvalidCredentialsLogin, InvalidUserLogin, SuccessfulLogin}
+import io.gatling.core.Predef._
+import io.gatling.http.Predef.http
+
+import scala.concurrent.duration.FiniteDuration
+
+class GerritOauthSimulation extends Simulation {
+
+  private val scenarios = List(
+    SuccessfulLogin,
+    InvalidCredentialsLogin,
+    InvalidUserLogin
+  )
+
+  private val protocol = http
+    .baseUrl(simulationConfig.httpUrl.get)
+    .disableFollowRedirect
+
+  val pauseStdDevSecs = 5
+  setUp(
+    scenarios.map(
+      _.scn
+        .inject(
+          rampConcurrentUsers(1) to simulationConfig.numUsers during simulationConfig.duration
+        )
+        .pauses(normalPausesWithStdDevDuration(FiniteDuration(pauseStdDevSecs, "seconds")))
+        .protocols(protocol)
+    )
+  ).protocols(protocol).maxDuration(simulationConfig.duration)
+}
---
+++ src/test/scala/gerritforge/scenarios/oauth/InvalidCredentialsLogin.scala
@@ -0,0 +1,35 @@
+package gerritforge.scenarios.oauth
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef._
+import org.jsoup._
+
+object InvalidCredentialsLogin extends OauthScenarioBase {
+
+  override def scnTitle: String = "A user with non-valid credentials cannot log into Gerrit"
+
+  override def scnActions: ChainBuilder =
+    exec(browseToGerritLogin)
+      .exec(setOauthRedirect())
+      .exec(
+        browseToOauthLogin
+      )
+      .exec { session =>
+        session.set("oauthCookies", session.attributes.getOrElse("cookies", "").asInstanceOf[List[String]].head)
+      }
+      .exec { session =>
+        session.set("action", Jsoup.parse(session.attributes.getOrElse("htmlform", "").asInstanceOf[String]).select("form").first().attributes().get("action"))
+      }
+      .exec(
+        http(scnTitle)
+          .post("#{action}")
+          .headers(Map("Content-Type" -> "application/x-www-form-urlencoded",
+            "Cookie" -> "#{oauthCookies}"))
+          .body(StringBody(getLoginCredentials(hasValidCredentials = false)))
+          .check(
+            status.is(200),
+          )
+      )
+      .pause(pauseDuration, pauseStdDev)
+}
