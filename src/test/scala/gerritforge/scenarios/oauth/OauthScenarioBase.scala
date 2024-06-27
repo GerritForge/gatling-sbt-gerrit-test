13d3a5ed73a98c2108224ca887ade6b5924e5a9d
Diego Zambelli Sessona <diego.sessona@gmail.com>
2024-06-27 22:02:51 +0200

    Add a simulation to test Gerrit oauth plugin
    The simulation GerritOauthSimulation adds functional tests
    to check that user can or cannot login into Gerrit based
    on the user existance and validity of its credentials in
    the oauth provider.
    
    Change-Id: I62f8e8c6b807ff174a8f890b3f21a6b2f2c3a790

---
+++ src/test/scala/gerritforge/scenarios/oauth/OauthScenarioBase.scala
@@ -0,0 +1,50 @@
+package gerritforge.scenarios.oauth
+
+import gerritforge.config.HttpConfig.httpConfig
+import gerritforge.config.OauthConfig.oauthConfig
+import gerritforge.config.SimulationConfig.simulationConfig
+import gerritforge.scenarios.ScenarioBase
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef._
+import io.gatling.http.request.builder.HttpRequestBuilder
+
+import scala.util.Random
+
+trait OauthScenarioBase extends ScenarioBase {
+
+  final val GERRIT_ACCOUNT_COOKIE_NAME = "GerritAccount"
+
+  def browseToGerritLogin: HttpRequestBuilder = {
+    http("Gerrit /login redirects to oauth provider")
+      .get(s"${simulationConfig.httpUrl.get}/login/%2Fq%2Fstatus%3Aopen%2B-is%3Awip")
+      .check(
+        status.is(302),
+        header("Location").saveAs("redirectToOauthProviderUrl")
+      )
+  }
+
+  def browseToOauthLogin: HttpRequestBuilder =
+    http("Oauth provider ask for login")
+      .get("#{oauthLoginUrl}")
+      .check(
+        status.is(200),
+        bodyString.saveAs("htmlform"),
+        header("set-cookie").findAll.saveAs("cookies")
+      )
+
+  def getLoginCredentials(
+      hasValidCredentials: Boolean = true
+  ): String = {
+    val pwd = if (hasValidCredentials) httpConfig.password else Random.alphanumeric.take(4).mkString
+    s"username=${httpConfig.username}&password=$pwd&credentialId="
+  }
+
+  def setOauthRedirect(): ChainBuilder = {
+    exec { session =>
+      val firstRedirect: String =
+        session.attributes.getOrElse("redirectToOauthProviderUrl", "").asInstanceOf[String]
+      session.set("oauthLoginUrl", firstRedirect.replace(oauthConfig.localUrl, oauthConfig.url))
+    }
+  }
+}
