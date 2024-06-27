62282e3b639ee777bb1296322f4d76fa9a816b45
Diego Zambelli Sessona <diego.sessona@gmail.com>
2024-06-27 22:02:51 +0200

    Add a simulation to test Gerrit oauth plugin
    The simulation GerritOauthSimulation adds functional tests
    to check that user can or cannot login into Gerrit based
    on the user existance and validity of its credentials in
    the oauth provider.
    
    Change-Id: I62f8e8c6b807ff174a8f890b3f21a6b2f2c3a790

---
+++ src/test/scala/gerritforge/scenarios/oauth/SuccessfulLogin.scala
@@ -0,0 +1,57 @@
+package gerritforge.scenarios.oauth
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef._
+import org.jsoup._
+
+object SuccessfulLogin extends OauthScenarioBase {
+
+  override def scnTitle: String = "A user with valid credentials logs into Gerrit"
+
+  override def scnActions: ChainBuilder =
+    exec(browseToGerritLogin)
+      .exec(browseToOauthLogin)
+      .exec { session =>
+        session.set(
+          "oauthCookies",
+          session.attributes.getOrElse("cookies", "").asInstanceOf[List[String]].head
+        )
+      }
+      .exec { session =>
+        session.set(
+          "action",
+          Jsoup
+            .parse(session.attributes.getOrElse("htmlform", "").asInstanceOf[String])
+            .select("form")
+            .first()
+            .attributes()
+            .get("action")
+        )
+      }
+      .exec(
+        http(scnTitle)
+          .post("#{action}")
+          .headers(
+            Map(
+              "Content-Type" -> "application/x-www-form-urlencoded",
+              "Cookie"       -> "#{oauthCookies}"
+            )
+          )
+          .body(StringBody(getLoginCredentials()))
+          .check(
+            status.is(302),
+            headerRegex("Location", s"^((.*)/oauth).*"),
+            header("Location").saveAs("redirectToGerritUrl")
+          )
+      )
+      .exec(
+        http("A valid user is redirected to Gerrit and is logged in")
+          .get("#{redirectToGerritUrl}")
+          .check(
+            status.is(302),
+            headerRegex("Set-Cookie", s"$GERRIT_ACCOUNT_COOKIE_NAME=([^;]*)").exists
+          )
+      )
+      .pause(pauseDuration, pauseType)
+}
